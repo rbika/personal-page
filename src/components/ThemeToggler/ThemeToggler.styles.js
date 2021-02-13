edee6f20a1fd130613c35206de3f061a2f965bd9
Rafael Bicalho <rbmbika@gmail.com>
2021-02-12 22:37:30 -0300

    Change Emotion syntax

--- src/components/ThemeToggler/ThemeToggler.styles.js
+++ src/components/ThemeToggler/ThemeToggler.styles.js
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 
-export const Container = styled.div(({ theme }) => ({
-  position: 'absolute',
-  right: '0.5rem',
-  top: '0.5rem',
-  cursor: 'pointer',
-  color: theme.colors.textPrimary,
-}))
+export const Container = styled.div`
+  position: absolute;
+  right: 0.5rem;
+  top: 0.5rem;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.textPrimary};
+`
